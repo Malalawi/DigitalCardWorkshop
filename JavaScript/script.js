e84821d6ea57aea9e532c7894d93a4b296e26e8f
Malalawi <alalawimarwa96@gmail.com>
2020-11-20 23:17:44 +0300

    Card-Design Template

---
+++ JavaScript/script.js
@@ -0,0 +1,17 @@
+   function myFunction() {
+        var envImg= document.getElementById("envelope");
+        var envMsg= document.getElementById("my-msg");
+        var card= document.getElementById("card");
+        envImg.style.visibility="hidden";
+       card.fadeTo("slow",1); 
+       
+   }
+
+
+function appera(){ 
+
+
+
+}
+
+
